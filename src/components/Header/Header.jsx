89b8fee0a51500587172ebd13bc3ae3caee8d7ec
Francisco Moreno <fmoreno@aligntech.com>
2025-06-25 12:23:02 +0200

    feat: exercise 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, FAMILIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -29,22 +29,27 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+        <ActionGroup>
+          <Column>
+            <Button>
+              subscribe
+            </Button>
+            <Link>Already a subscriber?</Link>
+          </Column>
+        </ActionGroup>
       </MainHeader>
     </header>
   );
 };
-
-const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
-`;
-
-const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
-`;
 
 const ActionGroup = styled.div`
   display: flex;
@@ -59,12 +64,51 @@ const ActionGroup = styled.div`
   }
 `;
 
+const SuperHeader = styled.div`
+  padding: 16px 0;
+  background: var(--color-gray-900);
+  color: white;
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
+`;
+
+const Row = styled(MaxWidthWrapper)`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  ${ActionGroup} {
+      display: none;
+  }
+  @media ${QUERIES.laptopAndUp} {
+  justify-content: space-between;
+  align-items: baseline;
+    ${ActionGroup} {
+      display: flex;
+    }
+  }
+`;
+
+const Link = styled.a`
+  font-family: ${FAMILIES.serif};
+  font-size: 1rem;
+  text-decoration: underline;
 `;
 
 export default Header;
