8dd6e4ac04fd481534908db4d2167a8120bbcaae
Francisco Moreno <fmoreno@aligntech.com>
2025-06-25 17:00:32 +0200

    feat: exercise 2
    opinion avatar placement

--- src/components/OpinionStory/OpinionStory.jsx
+++ src/components/OpinionStory/OpinionStory.jsx
@@ -17,9 +17,6 @@ const OpinionStory = ({ id, title, author, avatar, className }) => {
   );
 };
 
-const Wrapper = styled.article`
-  color: var(--color-gray-900);
-`;
 
 const Avatar = styled.img`
   display: block;
@@ -27,6 +24,23 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+`;
+
+const Wrapper = styled.article`
+  color: var(--color-gray-900);
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: space-between;
+  ${Avatar} {
+    margin-left: 16px;
+  }
+
+  @media ${QUERIES.tabletAndUp} {
+    flex-direction: column;
+    ${Avatar} {
+      margin-left: 0px;
+    }
+  }
 `;
 
 const AuthorName = styled.p`
